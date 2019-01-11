b1512a1f0bcae9041ea74cd678740735c48b85d1
Tianyi  Liang <terrence8556@gmail.com>
2019-01-10 17:56:06 -0700

    made this script fetch itself on github

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 
 print(requests.__version__)
 
-r = requests.get("http://www.google.com")
+r = requests.get("https://raw.githubusercontent.com/terrence85561/cmput404lab1/master/main.py")
 #print(dir(r))
 print(r.text)
-print(r.status_code)
+# print(r.status_code)
